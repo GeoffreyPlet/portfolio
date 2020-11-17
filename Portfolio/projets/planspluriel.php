8182a97b9139c8111a6508211b5abbaf5cc28617
GeoffreyPlet <geoffreyparle@gmail.com>
2020-11-17 13:04:03 +0100

    correction lien

---
+++ Portfolio/projets/planspluriel.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+    <title>Geoffrey Plet - Developpeur freelance, Amiens.</title>
+    <meta name="description" content="Développement de site web - Front & Back.">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="keywords" content="site internet, web, application web, développement, hébergement web, développement web, symfony, wordpress, magento, prestashop, intégration web, freelance">
+    <meta name="author" content="Geoffrey Plet">
+    <meta property="og:site_name" content="Geoffrey Plet">
+    <meta property="og:title" content="Geoffrey Plet - Developpeur freelance, Amiens.">
+    <meta property="og:description" content="Développement de site web - Front & Back.">
+    <meta property="og:image" content="./img/back.jpg">
+    <meta property="og:url" content="https://geoffreyplet.com/">
+    <link rel="icon" type="image/x-icon" href="./img/back.jpg">
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+
+    <!-- DEBUT Header -->
+        <header id="accueil">
+
+            <!-- DEBUT nav -->
+            <nav class="navbar navbar-expand-lg navbar-light bg-transparent" id="nav" style="background-color:rgb(108, 117, 125) !important">
+                <div class="container">
+                    <div class="cadre text-uppercase my-logo my-color-hover">
+                        <a href="../index.php">
+                            geoffrey
+                        </a>                   
+                    </div>
+                    <div class="cadre text-uppercase my-logo my-color-hover">
+                        <a href="../index.php">
+                            retour
+                        </a>                   
+                    </div>
+                </div>
+                
+            </nav>
+            <!-- FIN nav -->
+        </header>
+    <!-- DEBUT Header -->
+
+    <!-- DEBUT MAIN  -->
+        <main class="pt-5">
+            <div class="container pt-5" id="demos">
+                <div class="row flex-column">
+                    <h2>
+                        Objectif
+                    </h2>
+                    <p>Intégration des produit grâce à WordPress. Mise en page des produits.
+                    </p>
+                </div>
+                <div class="row">
+                    <h2 class="text-right w-100">
+                        Technologie
+                    </h2>
+                    <ul class="list-group" style="width: 100%; text-align: end;">
+                        <li class="list-item text-uppercase">
+                            css
+                        </li>
+                        <li class="list-item text-uppercase">
+                            html
+                        </li>
+                        <li class="list-item text-uppercase">
+                            bootstrap
+                        </li>
+                        <li class="list-item text-uppercase">
+                            WordPress
+                        </li>
+                    </ul>
+                </div>
+                <div class="row justify-content-center mt-3">
+                    <div class="col-lg-4 col-md-6">
+                        <a href="https://www.planspluriel.fr/catalogue/" target="_blank">
+                            <div class="cadre planspluriel">
+
+                            </div>
+                            <p>
+                                 Planspluriel
+                            </p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </main>
+    <!-- FIN MAIN  -->
+
+    <!-- DEBUT footer -->
+    <footer class="bg-dark pt-5" style="margin-top : 180px;">
+        <section class="container">
+            <div class="row text-white">
+                <div class="col-lg-4">
+                    <div class=" text-uppercase my-logo">
+                       <a href="#accueil">geoffrey</a> 
+                    </div>
+                    <p class="pt-4">
+                        Développeur full stack
+                    </p>
+                </div>
+                <div class="col-lg-8 text-center">
+                    <h3 class="text-uppercase">Adresse</h3>
+                    <ul class="d-flex justify-content-around flex-column">
+                        <li>53 Allée de la joie de vivre</li>
+                        <li>80000 Amiens</li>
+                        <li>geoffreyplett@gmail.com</li>
+                        <li>06.31.15.14.45</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+
+        
+               
+
+
+    </footer>
+    <!-- FIN footer -->
+
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
